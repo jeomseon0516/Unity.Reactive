99dc271d79cc8804929a560be57512fd1df95ac3
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-07-29 14:12:40 +0900

    docs: add basic usage sample

---
+++ Samples~/BasicUsage/ReactiveListSample.cs
@@ -0,0 +1,17 @@
+using Jeomseon.Reactive;
+using UnityEngine;
+
+namespace Jeomseon.Samples.Reactive
+{
+    public sealed class ReactiveListSample : MonoBehaviour
+    {
+        private readonly ReactiveList<string> _items = new();
+
+        private void Awake()
+        {
+            _items.AddListenerToAddedEventWithoutNotify(
+                (indices, items) => Debug.Log($"항목 추가: {items[0]}"));
+            _items.Add("Reactive Item");
+        }
+    }
+}
